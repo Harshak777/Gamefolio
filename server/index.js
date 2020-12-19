a27d22b59d4a991db830844237f433dadeef7a82
balasbk <balasbk1999@gmail.com>
2020-12-20 03:40:52 +0530

    adding contest_api and game_api

--- server/index.js
+++ server/index.js
@@ -1,28 +1,67 @@
 const express = require('express');
 
-const { sequelize,user } = require('./models');
+const { sequelize,game,contest } = require('./models');
 
 const app = express();
 app.use(express.json());
 
+//creating game
+app.post('/creategame', async(req, res) => {
+    const { name, rules, platform } = req.body;
 
-// app.post('/users', async(req, res) => {
-//     const { name, email, password } = req.body;
+try{
+   const insertinggameDetails= await game.create({ name, rules, platform });
 
-// try{
-//     await user.create({ name, email, password });
+    return res.json(insertinggameDetails);
+} catch(err) {
+    console.log(err);
+    return res.status(500).json(err);
+}
 
-//     return res.json(user);
-// } catch(err) {
-//     console.log(err);
-//     return res.status(500).json(err);
-// }
+});
 
-// });
+
+//creating contest
+app.post('/createcontest',async(req,res) => {
+    const{contestName,organiser,reward,venue,start,end,gname}=req.body;
+    try {
+        const gameid=await game.findOne({where: {name:gname}});
+        const contestdetails=await contest.create({contestName,organiser,reward,venue,start,end,gid:gameid.gid});
+        return res.json(contestdetails);
+    } catch (error) {
+     console.log(error);
+    return res.status(500).json(error);
+    }
+});
+
+//fetching contest
+
+app.get('/fetchcontest' ,async(req,res)=>{
+    try {
+        const allcontest= await contest.findAll();
+        return res.json(allcontest);
+    } catch (error) {
+        console.log(error);
+    return res.status(500).json(error);
+    }
+})
+
+//fetching game
+app.get('/fetchgame/:gid' ,async(req,res)=>{
+    const gid=req.params.gid;
+    try {
+        const gettinggameDetails= await game.findOne({where : {gid}});
+        return res.json(gettinggameDetails);
+    } catch (error) {
+        console.log(error);
+    return res.status(500).json(error);
+    }
+})
+
 
 app.listen({ port: 5000 }, async () => {
     console.log('Server listening on port 5000');
-    await sequelize.sync({force: true});
+    // await sequelize.sync({force: true});
     console.log('Database synced!');
 })
     
