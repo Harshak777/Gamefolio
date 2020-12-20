afec0e7127faede6de6b334dafee73b03bb13687
Harshak777 <Harshak777@users.noreply.github.com>
2020-12-20 10:30:15 +0530

    jsonwebtok

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 
 const { sequelize,user } = require('./models');
+const { signAccessToken } = require('./jwt_helper');
 
 const app = express();
 app.use(express.json());
@@ -13,9 +14,9 @@ try{
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    await user.create({ name, email, password: hashedPassword });
-
-    return res.json(user);
+    const savedUser = await user.create({ name, email, password: hashedPassword });
+    const accessToken = await signAccessToken(savedUser.uid);
+    return res.json({accessToken});
 } catch(err) {
     console.log(err);
     return res.status(500).json(err);
@@ -23,9 +24,20 @@ try{
 
 });
 
+app.post('/login', async(req, res) => {
+    try {
+        
+        await bcrypt.compare(password, this.password);
+
+    } catch (error) {
+        console.log(err);
+        return res.status(500).json(err);
+    };
+});
+
 app.listen({ port: 5000 }, async () => {
     console.log('Server listening on port 5000');
-    await sequelize.sync({ force: true });
+    await sequelize.sync();
     console.log('Database synced!');
 });
     
