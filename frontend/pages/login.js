5b68ea19e2798ab891960414ac9a074e5e2e6269
Harshak777 <Harshak777@users.noreply.github.com>
2021-01-06 17:59:43 +0530

    tweaked the layout

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -7,24 +7,28 @@ export default class login extends Component {
 
         return (
             <Layout>
-                <form>
-                    <fieldset>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                        </div>
-                        <div className="form-group">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <form>
+                            <fieldset>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputEmail1">Email address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputPassword1">Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                </div>
+                                <div className="form-group">
 
-                            <button type="submit" className="btn btn-primary">Login</button>
-                        </div>
-                        <Link href="/signup"><a className= "text-primary">Are you a new user?</a></Link>
-                    </fieldset>
-                </form>
+                                    <button type="submit" className="btn btn-primary">Login</button>
+                                </div>
+                                <Link href="/signup"><a className= "text-primary">Are you a new user?</a></Link>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
             </Layout>
 
         )
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -5,34 +5,37 @@ export default class signup extends Component {
     render() {
         return (
         <Layout>
-           <div className="g-signin2"  data-theme="dark"></div>
-            <hr className="my-4"></hr>
-             <form>
-                    <fieldset>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">User name</label>
-                            <input type="username" className="form-control" id="exampleInputPassword1" placeholder="username" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">Re enter Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Password" />
-                        </div>
-                        <div className="form-group">
+            <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <div className="g-signin2"  data-theme="dark"></div>
+                            <hr className="my-4"></hr>
+                            <form>
+                                <fieldset>
+                                    <div className="form-group">
+                                        <label htmlFor="exampleInputEmail1">Email address</label>
+                                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="exampleInputPassword1">User name</label>
+                                        <input type="username" className="form-control" id="exampleInputPassword1" placeholder="username" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="exampleInputPassword1">Password</label>
+                                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="exampleInputPassword1">Re enter Password</label>
+                                        <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Password" />
+                                    </div>
+                                    <div className="form-group">
 
-                            <button type="submit" className="btn btn-primary">Register</button>
-                        </div>
-                    </fieldset>
-                </form>
-                
+                                        <button type="submit" className="btn btn-primary">Register</button>
+                                    </div>
+                                </fieldset>
+                            </form>
+                    </div>
+                </div>
         </Layout>
         )
     }
